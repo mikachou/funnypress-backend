e454d0d38993a8149f51ac0cfac6fbbed111e7d9
Michael Schuh <michael.schuh.34@gmail.com>
2024-12-29 17:11:56 +0100

    Feed and Article entities and Feed CRUD

---
+++ app/Filament/Resources/FeedResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FeedResource\Pages;
+use App\Filament\Resources\FeedResource\RelationManagers;
+use App\Models\Feed;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class FeedResource extends Resource
+{
+    protected static ?string $model = Feed::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->maxLength(511),
+                Forms\Components\TextInput::make('url')
+                    ->required()
+                    ->maxLength(511),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('url')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFeeds::route('/'),
+            'create' => Pages\CreateFeed::route('/create'),
+            'edit' => Pages\EditFeed::route('/{record}/edit'),
+        ];
+    }
+}
